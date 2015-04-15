5207205c4c14c09945bac9cf03c84fb76fdf78e2
sh <shpark@norionsoft.com>
2015-04-16 02:54:10 +0900

    normal notification

--- app/src/main/java/com/shpark/androidnativesample/NotificationBuilder.java
+++ app/src/main/java/com/shpark/androidnativesample/NotificationBuilder.java
@@ -1,6 +1,7 @@
 package com.shpark.androidnativesample;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -14,6 +15,17 @@ import android.widget.RemoteViews;
 public class NotificationBuilder {
 
     private String TAG = "NotificationBuilder";
+
+    public void BuildOldNotification(Activity mainActivity) {
+        NotificationManager notificationManager = (NotificationManager)mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new Notification(R.drawable.icon, "Normal Notification", System.currentTimeMillis());
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notification.defaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE;
+        notification.number = 13;
+        PendingIntent pendingIntent = PendingIntent.getActivity(mainActivity, 0, new Intent(mainActivity, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setLatestEventInfo(mainActivity, "Normal Title", "Normal Summary", pendingIntent);
+        notificationManager.notify(1234, notification);
+    }
 
     public void BuildExpandedNotification(String title, String message, String ticker, int imageSource, int titleColor, int messageColor, String sound) {
 
